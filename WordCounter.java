a5573c02e694e7c19a0c377c7b501f77d0a2230a
Andrew Kiema <andrewkiema94@gmail.com>
2024-12-19 12:47:50 +0300

    Add help notes

--- WordCounter.java
+++ WordCounter.java
@@ -7,14 +7,14 @@ public class WordCounter{
         Scanner sc=new Scanner(System.in);
         String fpath=sc.nextLine();
         File fl=new File(fpath);   //Holds file path
-        Scanner filescanner=new Scanner(fl);
+        Scanner filescanner=new Scanner(fl); //Reads file
         StringBuilder text=new StringBuilder();
         while(filescanner.hasNextLine()){
-            text.append(filescanner.nextLine()).append("\n");
+            text.append(filescanner.nextLine()).append("\n"); //"\n" means moving to another line
         }
         int characters=text.toString().length();
-        int words=text.toString().split("\\s+").length;
-        int sentences=text.toString().split("!.?").length;
+        int words=text.toString().split("\\s+").length; //"\\s+" means white spaces
+        int sentences=text.toString().split("!.?").length; //"!.?" means anywhere where there is a mark that signifies a new sentence
 
         System.out.println("This file has:");
         System.out.println(characters+" characters");
